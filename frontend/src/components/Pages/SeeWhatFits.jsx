f90bde696888deb538f56fba979cce3d8448e3e7
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-01-02 15:45:28 -0600

    See What Fits tabs

--- frontend/src/components/Pages/SeeWhatFits.jsx
+++ frontend/src/components/Pages/SeeWhatFits.jsx
@@ -1,9 +1,11 @@
-import react, { useContext } from "react";
+import react, { useContext, useState } from "react";
 import { AppContext } from "../../Providers/ContextProvider";
 import CloseButton from "../CloseButton";
 
 export default function SeeWhatFits() {
   const { activePage } = useContext(AppContext);
+  const [activeTab, setActiveTab] = useState("Foods");
+
   return (
     <div
       style={{}}
@@ -13,6 +15,50 @@ export default function SeeWhatFits() {
       }
     >
       <CloseButton functionList={[() => {}]} />
+
+      <h1>
+        See What Fits
+        <hr />
+      </h1>
+
+      <div>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+          <div>
+            <h4
+              onClick={() => setActiveTab("Foods")}
+              style={{
+                textAlign: "center",
+                backgroundColor:
+                  activeTab == "Foods" ? "#00000025" : "#00000008",
+                borderRadius: "4px",
+                padding: "6px",
+                margin: "0",
+                borderRight: activeTab == "Foods" ? "1px solid" : "0px",
+                borderBottom: "1px solid",
+              }}
+            >
+              Foods
+            </h4>
+          </div>
+          <div>
+            <h4
+              onClick={() => setActiveTab("Meals")}
+              style={{
+                textAlign: "center",
+                backgroundColor:
+                  activeTab == "Meals" ? "#00000025" : "#00000008",
+                borderRadius: "4px",
+                padding: "6px",
+                margin: "0",
+                borderLeft: activeTab == "Meals" ? "1px solid" : "0px",
+                borderBottom: "1px solid",
+              }}
+            >
+              Meals
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
