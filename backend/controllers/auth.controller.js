f5456224aaec1396478349a060a3b72056cb20e7
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-01-07 19:41:51 -0600

    password hashing and salting

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,43 @@
+import bcrypt from "bcrypt";
 import userData from "../models/users.model.js";
+
+const saltRounds = 10;
 
 export const registerUser = async (req, res) => {
   const userInfo = req.body;
   const userSearch = await userData.findOne({ username: userInfo.username });
-  if (userSearch) { // login
-    if (userInfo.password === userSearch._doc.password) {
-        res.status(200).json({ existingUser: true, message: "Logged In" });
-    } else {
-        res.status(200).json({ existingUser: true, message: "Failed Log In"})
-    }
-  } else { // register
-    const newUser = new userData(userInfo);
-    newUser.save();
+  if (userSearch) {
+    // login
+    bcrypt.compare(
+      userInfo.password,
+      userSearch._doc.password,
+      (err, result) => {
+        if (err) {
+          console.log("Error comparing passwords: ", err);
+          res.status(500).json({message: err});
+        } else {
+          if (result) {
+            res.status(200).json({ existingUser: true, message: "Logged In" });
+          } else {
+            res
+              .status(200)
+              .json({ existingUser: true, message: "Failed Log In" });
+          }
+        }
+      }
+    );
+  } else {
+    // register
+    bcrypt.hash(userInfo.password, saltRounds, async (err, hash) => {
+      if (err) {
+        console.log("Error hashing: ", err);
+        res.status(500).json({message: err});
+      } else {
+        const newUser = new userData({ ...userInfo, password: hash });
+        newUser.save();
+      }
+    });
+
     res.status(200).json({ existingUser: false, message: "User Registered" });
   }
 };
